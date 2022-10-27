4a8388b622e65d7b7abe97b23297be24fcc8009d
Tariq Ridwan <tariq.ridwan@bsc.es>
2022-10-27 17:10:52 +0200

    added 2nd lesson

--- cfdpy1.py
+++ cfdpy1.py
@@ -1,10 +1,7 @@
-# import matplotlib as mpl
-# from mpl_toolkits.mplot3d import Axes3D
-# import numpy as np
-# import matplotlib.pyplot as plt
 # Remember: comments in python are denoted by the pound sign
 import numpy                       #here we load numpy
 from matplotlib import pyplot      #here we load matplotlib
+import matplotlib.pyplot as plt #I ADDED THIS
 import time, sys                   #and load some utilities
 #this makes matplotlib plots appear in the notebook (instead of a separate window)
 #%matplotlib inline 
@@ -16,14 +13,14 @@ c = 1      #assume wavespeed of c = 1
 u = numpy.ones(nx)      #numpy function ones()
 u[int(.5 / dx):int(1 / dx + 1)] = 2  #setting u = 2 between 0.5 and 1 as per our I.C.s
 print(u)
-pyplot.plot(numpy.linspace(0, 2, nx), u);
-# un = numpy.ones(nx) #initialize a temporary array
+# pyplot.plot(numpy.linspace(0, 2, nx), u);
+# plt.show()
+un = numpy.ones(nx) #initialize a temporary array
 
-# for n in range(nt):  #loop for values of n from 0 to nt, so it will run nt times
-#     un = u.copy() ##copy the existing values of u into un
-#     for i in range(1, nx): ## you can try commenting this line and...
-#     #for i in range(nx): ## ... uncommenting this line and see what happens!
-#         u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
-#         pyplot.plot(numpy.linspace(0, 2, nx), u);
-        
-#         # plt.show()
+for n in range(nt):  #loop for values of n from 0 to nt, so it will run nt times
+    un = u.copy() ##copy the existing values of u into un
+    for i in range(1, nx): ## you can try commenting this line and...
+    #for i in range(nx): ## ... uncommenting this line and see what happens!
+        u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
+pyplot.plot(numpy.linspace(0, 2, nx), u);
+plt.show()
